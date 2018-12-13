79e2cd8f066209d8c7ee2b851b8b8627eddc808f
Peyman <peymanc@hotmail.com>
2018-12-13 09:59:45 -0500

    added features

--- src/app/recipe/recipes.model.ts
+++ src/app/recipe/recipes.model.ts
@@ -4,6 +4,7 @@ export class Recipe {
     public imagePath: string;
 
     //constructor runs when this object is instantiated
+    //the constructor method allows you to insantiate this object with the "new" keyword where you set the object in your code
     constructor(name: string, desc:string, imagePath: string) {
         this.name = name;
         this.description = desc;
