cb479016be482b043359f32f9fcca99b769529b3
Peyman-PC <peymanc@hotmail.com>
2020-05-28 12:13:47 -0400

    fixed merge conflicts 2

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,33 +1,11 @@
-<<<<<<< HEAD
-import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
-import { ShoppingListComponent } from "./shopping-list.component";
-import { NgModule } from "@angular/core";
-=======
 import { NgModule } from "@angular/core";
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
->>>>>>> f36342aca672f90691165695b8fa33c647e2f299
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
-<<<<<<< HEAD
-    declarations: [
-        ShoppingEditComponent,
-        ShoppingListComponent
-    ],
-    imports: [
-        CommonModule,
-        FormsModule,
-        RouterModule.forChild([
-            { path: 'shopping-list', component: ShoppingListComponent}
-        ])
-    ]
-})
-
-export class ShoppingListModule {]
-=======
   declarations: [
     ShoppingListComponent,
     ShoppingEditComponent,
@@ -42,4 +20,3 @@ export class ShoppingListModule {]
 })
 
 export class ShoppingListModule {}
->>>>>>> f36342aca672f90691165695b8fa33c647e2f299
